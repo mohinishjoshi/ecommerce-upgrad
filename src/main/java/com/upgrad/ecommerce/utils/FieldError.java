67ef84224a932e89e8fafbb0d8e5601ff428e8f6
Mohinish Joshi <mohinishjoshi@gmail.com>
2022-12-23 17:59:49 +0530

    Remove Unused code

--- src/main/java/com/upgrad/ecommerce/utils/FieldError.java
+++ src/main/java/com/upgrad/ecommerce/utils/FieldError.java
@@ -1,25 +1,12 @@
 package com.upgrad.ecommerce.utils;
 
 
+import lombok.Data;
+
+@Data
 public class FieldError {
 
     private String field;
     private String errorCode;
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(final String field) {
-        this.field = field;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(final String errorCode) {
-        this.errorCode = errorCode;
-    }
 
 }
