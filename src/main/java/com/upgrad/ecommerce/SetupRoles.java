020f216cb93625f048a270f830c711a7fc30eb8d
Mohinish Joshi <mohinishjoshi@gmail.com>
2022-12-26 18:42:07 +0530

    Add Missing Role files

---
+++ src/main/java/com/upgrad/ecommerce/SetupRoles.java
@@ -0,0 +1,31 @@
+package com.upgrad.ecommerce;
+
+import com.upgrad.ecommerce.models.ERole;
+import com.upgrad.ecommerce.services.RoleService;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SetupRoles implements ApplicationListener<ApplicationReadyEvent> {
+    private final List<ERole> roles = List.of(ERole.ADMIN, ERole.USER);
+
+    private final RoleService roleService;
+
+    public SetupRoles(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        for (ERole role : roles) {
+            try {
+                roleService.create(role);
+            } catch (Exception e) {
+                System.out.println("Skipping Role Creation");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/upgrad/ecommerce/services/RoleService.java
@@ -0,0 +1,26 @@
+package com.upgrad.ecommerce.services;
+
+import com.upgrad.ecommerce.models.ERole;
+import com.upgrad.ecommerce.models.Role;
+import com.upgrad.ecommerce.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleService(final RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public void create(final ERole roleName) {
+        Optional<Role> roleByName = roleRepository.findByName(roleName);
+        if (roleByName == null) {
+            Role role = new Role();
+            role.setName(roleName.name());
+            roleRepository.save(role);
+        }
+    }
+}
